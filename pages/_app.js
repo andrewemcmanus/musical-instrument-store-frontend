2989d20bbd4c5ff078518d503dda28f5ab46c77c
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-09-06 17:21:24 -0400

    begin _app.js build

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,33 @@
-import '../styles/globals.css'
+import Page from "../components/Page"
+import { ApolloProvider } from "@apollo/client";
+import { CartStateProvider } from "../lib/cartState";
+import Router from 'next/Router';
+import NProgress from "nprogress";
+import withData from '../lib/withData';
+// import '../components/styles/nprogress.css'
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps, apollo }) {
+  return (
+    <ApolloProvider>
+      <CartStateProvider>
+        <Page>
+          <Component {...pageProps} />
+        </Page>
+      </CartStateProvider>
+    </ApolloProvider>
+  ) 
 }
 
-export default MyApp
+MyApp.getInitialProps = async function({ Component, ctx }) {
+  let pageProps = {};
+  if(Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  };
+  pageProps.query = ctx.query;
+  return { pageProps };
+}
+
+export default withData(MyApp);
