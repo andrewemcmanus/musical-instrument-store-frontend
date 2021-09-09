b70353381e08c64582193e0b0f898972fa03c940
Andrew McManus <andrewmcmanusmusic@gmail.com>
2021-09-09 13:39:42 -0400

    SignOut

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,7 @@
 import Link from 'next/Link';
 import NavStyles from './styles/NavStyles';
-import Signout from './SignOut';
-// import { useUser } from './User';
+import SignOut from './SignOut';
+import { useUser } from './User';
 import { useCart } from '../lib/cartState';
 import CartCount from './CartCount';
 
